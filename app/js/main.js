349cf4b9a00379368b1f24e76406850034438c58
Peresadchenko <1pers1@bk.ru>
2021-11-27 22:26:02 +0300

    add swiper

--- app/js/main.js
+++ app/js/main.js
@@ -8,5 +8,33 @@ $(function () {
     autoplaySpeed: 2000
   });
 
-  
+  // $('.specialists__items').slick({
+  //   arrows: true,
+  //   adaptiveHeight: true,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   speed: 1000,
+  //   easing: 'ease',
+  //   // autoplay: true,
+  //   autoplaySpeed: 2000
+  // });
+
+  new Swiper('.swiper', {
+    navigation: {
+      nextEl: '.swiper-next',
+      prevEl: '.swiper-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+      // clickable: true,
+    //   dynamicBullets: true,
+    //   renderBullet: function (index, className) {
+    //     return '<span class="' + className + '">' + (index + 1) + '</span>';
+    //   },
+    },
+
+  });
+
 });
+
