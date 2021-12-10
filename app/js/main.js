afb4f064a58806edf9a907d63fc3165230bcd41e
Peresadchenko <1pers1@bk.ru>
2021-12-10 19:54:26 +0300

    the end

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,7 @@ $(function () {
     dots: true,
     arrows: false,
     fade: true,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 2000
   });
 
@@ -20,92 +20,165 @@ $(function () {
     slidesToScroll: 1,
     speed: 1000,
     easing: 'ease',
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 2000
   });
 
   $('.video__slider-wrapper').slick({
+    responsive: [
+      {
+        breakpoint: 1150,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },    
+    ],
     arrows: false,
     adaptiveHeight: true,
-    slidesToShow: 1.35,
+    slidesToShow: 1.9,
     slidesToScroll: 1,
     infinite: false,
     speed: 1000,
     easing: 'ease',
     centerMode: true,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 2000
   });
 
   $('.news__slider').slick({
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },    
+    ],
     arrows: false,
     adaptiveHeight: true,
     slidesToShow: 3,
     slidesToScroll: 3,
     speed: 1000,
     easing: 'ease',
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 2000
   });
 
   $('.social__slider').slick({
+    responsive: [
+      {
+        breakpoint: 1150,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },    
+    ],
     arrows: false,
     adaptiveHeight: true,
     slidesToShow: 3,
     slidesToScroll: 3,
     speed: 1000,
     easing: 'ease',
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 2000
   });
 
-  $('.slider-item__content-btnPrev, .video__slider-btnPrev').click(function (event) {
-    $('.reviews__slider, .video__slider-wrapper').slick("slickPrev");
-  });
-  $('.slider-item__content-btnNext, .video__slider-btnNext').click(function (event) {
-    $('.reviews__slider, .video__slider-wrapper').slick("slickNext");
+  $('.specialists__slider').slick({
+    responsive: [
+      {
+        breakpoint: 1150,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 590,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }         
+    ],
+    arrows: false,
+    // variableWidth: true,
+    adaptiveHeight: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    speed: 1000,
+    easing: 'ease',
+    autoplay: true,
+    autoplaySpeed: 2000
+    
   });
 
-  $('.news__slider-btnPrev').click(function (event) {
-    $('.news__slider').slick("slickPrev");
-  });
-  $('.news__slider-btnNext').click(function (event) {
-    $('.news__slider').slick("slickNext");
-  });
+$('.slider-item__content-btnPrev, .video__slider-btnPrev').click(function (event) {
+  $('.reviews__slider, .video__slider-wrapper').slick("slickPrev");
+});
+$('.slider-item__content-btnNext, .video__slider-btnNext').click(function (event) {
+  $('.reviews__slider, .video__slider-wrapper').slick("slickNext");
+});
 
-  $('.social__slider-btnPrev').click(function (event) {
-    $('.social__slider').slick("slickPrev");
-  });
-  $('.social__slider-btnNext').click(function (event) {
-    $('.social__slider').slick("slickNext");
-  });
+$('.news__slider-btnPrev').click(function (event) {
+  $('.news__slider').slick("slickPrev");
+});
+$('.news__slider-btnNext').click(function (event) {
+  $('.news__slider').slick("slickNext");
+});
 
-  var mixer = mixitup('.clinics', {
-    load: {
-      filter: '.category-a'
-    }
-  });
+$('.social__slider-btnPrev').click(function (event) {
+  $('.social__slider').slick("slickPrev");
+});
+$('.social__slider-btnNext').click(function (event) {
+  $('.social__slider').slick("slickNext");
+});
+
+$('.specialists__slider-btnPrev').click(function (event) {
+  $('.specialists__slider').slick("slickPrev");
+});
+$('.specialists__slider-btnNext').click(function (event) {
+  $('.specialists__slider').slick("slickNext");
+});
+
+var mixer = mixitup('.clinics', {
+  load: {
+    filter: '.category-a'
+  }
+});
 
 
-  $(".star").rateYo({
-    starWidth: "33px",
-    spacing: "13px",
-    normalFill: "#d6d6d6",
-    ratedFill: "#FBBC05",
-    readOnly: true
-  });
+$(".star").rateYo({
+  starWidth: "33px",
+  spacing: "13px",
+  normalFill: "#d6d6d6",
+  ratedFill: "#FBBC05",
+  readOnly: true
+});
 
 
-
-  new Swiper('.swiper', {
-    navigation: {
-      nextEl: '.swiper-next',
-      prevEl: '.swiper-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'fraction',
-    },
-  });  
 });
 
